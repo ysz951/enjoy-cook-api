cfc118d7c0478fff044ba914db0d978f97967166
ysz951 <shengyang_zhou@baylor.edu>
2020-08-10 04:52:06 -0500

    add migrations and seeds

---
+++ seeds/seed.enjoycook_tables.sql
@@ -0,0 +1,34 @@
+BEGIN;
+
+TRUNCATE
+  enjoycook_categories,
+  enjoycook_users,
+  enjoycook_recipes,
+  enjoycook_comments
+  RESTART IDENTITY CASCADE;
+
+INSERT INTO enjoycook_categories (name)
+VALUES
+  ('Breakfast'),
+  ('Lunch');
+
+INSERT INTO enjoycook_users (user_name, full_name, nickname, password)
+VALUES
+  ('bruce', 'Bruce Wayne', null, 'Batman!'),
+  ('stark', 'Tony Stark', 'tony', 'Ironman!');
+
+INSERT INTO enjoycook_recipes (name, content, author_id, category_id)
+VALUES
+  ('Fried Egg', 'Corporis accusamus placeat quas non voluptas. Harum fugit molestias qui. Velit ex animi reiciendis quasi. Suscipit totam delectus ut voluptas aut qui rerum. Non veniam eius molestiae rerum quam.\n \rUnde qui aperiam praesentium alias. Aut temporibus id quidem recusandae voluptatem ut eum. Consequatur asperiores et in quisquam corporis maxime dolorem soluta. Et officiis id est quia sunt qui iste reiciendis saepe. Ut aut doloribus minus non nisi vel corporis. Veritatis mollitia et molestias voluptas neque aspernatur reprehenderit.\n \rMaxime aut reprehenderit mollitia quia eos sit fugiat exercitationem. Minima dolore soluta. Quidem fuga ut sit voluptas nihil sunt aliquam dignissimos. Ex autem nemo quisquam voluptas consequuntur et necessitatibus minima velit. Consequatur quia quis tempora minima. Aut qui dolor et dignissimos ut repellat quas ad.', 1, 1),
+  ( 'teak', 'Eos laudantium quia ab blanditiis temporibus necessitatibus. Culpa et voluptas ut sed commodi. Est qui ducimus id. Beatae sint aspernatur error ullam quae illum sint eum. Voluptas corrupti praesentium soluta cumque illo impedit vero omnis nisi.\n \rNam sunt reprehenderit soluta quis explicabo impedit id. Repudiandae eligendi libero ad ut dolores. Laborum nihil sint et labore iusto reiciendis cum. Repellat quos recusandae natus nobis ullam autem veniam id.\n \rEsse blanditiis neque tempore ex voluptate commodi nemo. Velit sapiente at placeat eveniet ut rem. Quidem harum ut dignissimos. Omnis pariatur quas aperiam. Quasi voluptas qui nulla. Iure quas veniam aut quia et.', 2, 2);
+        
+INSERT INTO enjoycook_comments (content, recipe_id, user_id) 
+VALUES
+  ('Repudiandae eligendi libero ad ut dolores. Laborum nihil sint et labore iusto reiciendis cum. Repellat quos recusandae natus nobis ullam autem veniam id.\n \rEsse blanditiis neque tempore ex voluptate commodi nemo. Velit sapiente at placeat eveniet ut rem. Quidem harum ut dignissimos. Omnis pariatur quas aperiam. Quasi voluptas qui nulla. Iure quas veniam aut quia et.', 1, 1),
+  ('Veritatis mollitia et molestias voluptas neque aspernatur reprehenderit.\n \rMaxime aut reprehenderit mollitia quia eos sit fugiat exercitationem. Minima dolore soluta. Quidem fuga ut sit voluptas nihil sunt aliquam dignissimos. Ex autem nemo quisquam voluptas consequuntur et necessitatibus minima velit. Consequatur quia quis tempora minima. Aut qui dolor et dignissimos ut repellat quas ad.', 2, 2),
+  ('Ex autem nemo quisquam voluptas consequuntur et necessitatibus minima velit. Consequatur quia quis tempora minima. Aut qui dolor et dignissimos ut repellat quas ad.', 1, 2),
+  ('eligendi libero ad ut dolores. Laborum nihil sint', 2, 1);
+        
+COMMIT;
+
+        
