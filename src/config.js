0d1538b256e7f15962f6add21abf76980adac157
ysz951 <shengyang_zhou@baylor.edu>
2020-08-17 05:24:10 -0500

    change env

--- src/config.js
+++ src/config.js
@@ -3,6 +3,6 @@ module.exports = {
     NODE_ENV: process.env.NODE_ENV || "development",
     DATABASE_URL: process.env.DATABASE_URL || "postgresql://enjoycook_server@localhost/enjoycook",
     TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || "postgresql://enjoycook_server@localhost/enjoycook-test",
-    JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
+    JWT_SECRET: process.env.JWT_SECRET,
     JWT_EXPIRY: process.env.JWT_EXPIRY || '3h',
   }
